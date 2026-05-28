2d1a6a6b6d31d7012778bac0089a4fda7cfdbadb
Umesh Pal <umeshpalshispn@gmail.com>
2026-05-28 09:19:05 +0000

    remove env.js from repo

--- env.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBpn16zqfxH_D4WwLtuAwDGQdQ3-qMsdCc",
-  authDomain: "civic-issue-reporting-to-b6f3d.firebaseapp.com",
-  projectId: "civic-issue-reporting-to-b6f3d",
-  storageBucket: "civic-issue-reporting-to-b6f3d.firebasestorage.app",
-  messagingSenderId: "442755472602",
-  appId: "1:442755472602:web:d819b368244377072f59e3"
-};
